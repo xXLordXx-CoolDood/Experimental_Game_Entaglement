3579e073b05f3620bbb4b83d2ec797df7a882e54
ahsan2649 <ahsan2649@outlook.com>
2023-05-30 13:51:34 +0200

    Player Movement Finished

--- Assets/Ahsan/Uncontrol Room/Scripts/Movement.cs
+++ Assets/Ahsan/Uncontrol Room/Scripts/Movement.cs
@@ -13,6 +13,8 @@ public class Movement : MonoBehaviour
 
     Vector2 m_Movement;
     Vector2 m_Look;
+
+    float lookValue;
     private void OnEnable()
     {
         Move.Enable();
@@ -29,18 +31,33 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        lookValue = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        // Read Input
         m_Movement = Move.ReadValue<Vector2>();
         m_Look = Look.ReadValue<Vector2>();
 
+        // Move Character
         transform.Translate(new Vector3(m_Movement.x, 0, m_Movement.y) * Time.deltaTime * moveSpeed, Space.Self);
 
-        transform.Rotate(transform.up, m_Look.x, Space.World);
+        // Move Camera
+        transform.Rotate(transform.up, m_Look.x * camSpeed, Space.World);
+        lookValue -= m_Look.y * camSpeed;
+        lookValue = Mathf.Clamp(lookValue, -90.0f, 90.0f);
+        var CamTransform = GetComponentInChildren<Camera>().transform;
+        CamTransform.rotation = Quaternion.Euler(lookValue,CamTransform.eulerAngles.y,0);
+
+
+
+
+
+
+
+
+
     }
 }
