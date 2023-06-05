085bbf448140927e97fa5802dcacbb77ef24d95b
xXLordXx-CoolDood <langlandsx@gmail.com>
2023-06-05 18:02:39 +0200

    Height & tilt added

--- Assets/PROTO_Dog_Controller.cs
+++ Assets/PROTO_Dog_Controller.cs
@@ -6,41 +6,51 @@ using UnityEngine.InputSystem;
 public class PROTO_Dog_Controller : MonoBehaviour
 {
     public PROTO_Leg_Animator LF, LB, RF, RB;
-    public float legSpeed = 1, maxLegHeight = 1;
+    public Transform direction;
+    public float legSpeed = 1, maxLegHeight = 1, heightOffset = 1, rotationMultiplier;
+
+    [HideInInspector] public int engagedLegs;
+    [HideInInspector] public bool opposites;
 
     private PlayerInput playerInput;
+    private int directionChange;
     private bool LFHeld, LBHeld, RFHeld, RBHeld;
-    [SerializeField] private bool opposites;
 
     void Start()
     {
+        //Initialize vars and legs
         playerInput = new PlayerInput();
-        LF.legSpeed = legSpeed; LF.maxLegHeight = maxLegHeight;
-        LB.legSpeed = legSpeed; LB.maxLegHeight = maxLegHeight;
-        RF.legSpeed = legSpeed; RF.maxLegHeight = maxLegHeight;
-        RB.legSpeed = legSpeed; RB.maxLegHeight = maxLegHeight;
+        LF.legSpeed = legSpeed; LF.controllerRef = this;
+        LB.legSpeed = legSpeed; LB.controllerRef = this;
+        RF.legSpeed = legSpeed; RF.controllerRef = this;
+        RB.legSpeed = legSpeed; RB.controllerRef = this;
     }
 
     #region
     public void ForwardLeft(InputAction.CallbackContext ctx) 
     {
-        if (ctx.performed) { LF.isHeld = true; }
-        if (ctx.canceled) { LF.isHeld = false; }
+        if (ctx.performed) { LF.isHeld = true; engagedLegs++; }
+        if (ctx.canceled) { LF.isHeld = false; engagedLegs--; }
     }
     public void BackwardLeft(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed) { LB.isHeld = true; }
-        if (ctx.canceled) { LB.isHeld = false; }
+        if (ctx.performed) { LB.isHeld = true; engagedLegs++; }
+        if (ctx.canceled) { LB.isHeld = false; engagedLegs--; }
     }
     public void ForwardRight(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed) { RF.isHeld = true; }
-        if (ctx.canceled) { RF.isHeld = false; }
+        if (ctx.performed) { RF.isHeld = true; engagedLegs++; }
+        if (ctx.canceled) { RF.isHeld = false; engagedLegs--; }
     }
     public void BackwardRight(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed) { RB.isHeld = true; }
-        if (ctx.canceled) { RB.isHeld = false; }
+        if (ctx.performed) { RB.isHeld = true; engagedLegs++; }
+        if (ctx.canceled) { RB.isHeld = false; engagedLegs--; }
+    }
+    public void Turn(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed) { directionChange = Mathf.RoundToInt(ctx.ReadValue<float>()); }
+        if (ctx.canceled) { directionChange = 0; }
     }
     #endregion //Input Handling
 
@@ -48,8 +58,11 @@ public class PROTO_Dog_Controller : MonoBehaviour
     {
         opposites = false;
 
+        Vector3 delta = new Vector3(0, directionChange, 0);
+        direction.Rotate(delta, Time.deltaTime * 20);
+
         //Check if opposite legs are active
-        if(RB.isHeld && LF.isHeld && !RF.isHeld && !LB.isHeld) {
+        if (RB.isHeld && LF.isHeld && !RF.isHeld && !LB.isHeld) {
             opposites = true;
             RB.canMove = true;
             LF.canMove = true;
@@ -62,12 +75,20 @@ public class PROTO_Dog_Controller : MonoBehaviour
         }
 
         MoveBody();
+        RotateBody();
     }
 
     void MoveBody()
     {
         float averageZ = (LF.legTarget.position.z + LB.legTarget.position.z + RF.legTarget.position.z + RB.legTarget.position.z) / 4;
+        float averageY = (LF.legTarget.position.y + LB.legTarget.position.y + RF.legTarget.position.y + RB.legTarget.position.y) / 4;
+        maxLegHeight = (averageY + heightOffset) * 0.9f;
+        transform.position = new Vector3(transform.position.x, averageY + heightOffset, averageZ);
+    }
 
-        transform.position = new Vector3(transform.position.x, transform.position.y, averageZ);
+    void RotateBody()
+    {
+        float angle = (LB.legTarget.position.y + RB.legTarget.position.y) - (LF.legTarget.position.y + RF.legTarget.position.y);
+        transform.eulerAngles = new Vector3(angle * rotationMultiplier, 0, 0);
     }
 }
--- Assets/Xavier/PROTO_Leg_Animator.cs
+++ Assets/Xavier/PROTO_Leg_Animator.cs
@@ -8,14 +8,15 @@ public class PROTO_Leg_Animator : MonoBehaviour
     public LayerMask groundLayer;
     public AnimationCurve legHeightCurve;
 
-    [HideInInspector] public bool isHeld, canMove;
-    [HideInInspector] public float legSpeed, maxLegHeight;
+    [HideInInspector] public PROTO_Dog_Controller controllerRef;
+    [HideInInspector] public bool isHeld, canMove, grounded;
+    [HideInInspector] public float legSpeed;
 
     float maxLegDistance;
 
     void Awake()
     {
-        legTarget.parent = null;
+        //legTarget.parent = transform.parent.parent.parent;
 
         maxLegDistance = Vector3.Distance(root.position, mid.position) + Vector3.Distance(mid.position, legTarget.position);
     }
@@ -30,30 +31,29 @@ public class PROTO_Leg_Animator : MonoBehaviour
     void CheckForGround()
     {
         RaycastHit hit;
-
+        grounded = false;
         Vector3 checkHeight = new Vector3(legTarget.position.x, legTarget.position.y + maxLegDistance, legTarget.position.z);
         if (Physics.Raycast(checkHeight, -Vector3.up, out hit, maxLegDistance, groundLayer))
         {
             legTarget.position = hit.point;
             canMove = false;
+            grounded = true;
         }
     }
 
     void UpdateLeg()
     {
-        float time = legTarget.position.y / maxLegHeight;
-
-        if (isHeld) { 
+        if (isHeld && (controllerRef.engagedLegs == 1 || controllerRef.opposites)) {
             legTarget.transform.position = new Vector3(legTarget.position.x, Mathf.Clamp(legTarget.position.y + (Time.deltaTime * legSpeed)
-                , -maxLegHeight, maxLegHeight), legTarget.position.z);
+                , -controllerRef.maxLegHeight, controllerRef.maxLegHeight), legTarget.position.z);
         }
         else if(canMove){
             legTarget.transform.position = new Vector3(legTarget.position.x, Mathf.Clamp(legTarget.position.y - (Time.deltaTime * legSpeed
-                 ), -maxLegHeight, maxLegHeight), legTarget.position.z + (Time.deltaTime * legSpeed));
+                 ), -controllerRef.maxLegHeight, controllerRef.maxLegHeight), legTarget.position.z + (Time.deltaTime * legSpeed));
         }
-        else {
+        else if(!grounded){
             legTarget.transform.position = new Vector3(legTarget.position.x, Mathf.Clamp(legTarget.position.y - (Time.deltaTime * legSpeed)
-                , -maxLegHeight, maxLegHeight), legTarget.position.z);
+                , -controllerRef.maxLegHeight, controllerRef.maxLegHeight), legTarget.position.z);
         }
     }
 }
