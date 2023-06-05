ffbfdc71b9573a205935e59f8a2e19eb2629b5d0
xXLordXx-CoolDood <langlandsx@gmail.com>
2023-06-05 12:44:40 +0200

    Cringe Movement

--- Assets/PROTO_DogLikeMech.cs
+++ Assets/PROTO_DogLikeMech.cs
@@ -5,57 +5,126 @@ using UnityEngine.InputSystem;
 
 public class PROTO_DogLikeMech : MonoBehaviour
 {
-    public List<Transform> legTargets = new List<Transform>(4);
+    public Transform FL, BL, FR, BR, myBody;
     public float maxLegHeight = 3f;
     public float legSpeed;
+    public float rotationMultiplier = 1;
+    public LayerMask groundLayer;
+    public float maxLegLength;
 
     private PlayerInput playerInput;
-    private List<bool> legActive = new List<bool>(4);
+    private bool FLB, BLB, FRB, BRB;
+    private bool FLgrounded, BLgrounded, FRgrounded, BRgrounded;
+    private float offsetY, offsetZ;
+    private float legHeightOffset;
+    private int groundedLegs;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        playerInput = GetComponent<PlayerInput>();
+        //float legLength = Vector3.Distance(b1.position, b2.position);
+        //legLength += Vector3.Distance(b2.position, b3.position);
+        //legLength += Vector3.Distance(b3.position, b4.position);
+        //Debug.Log(legLength);
 
-        for(int i = 0; i < legTargets.Count; i++) { legActive.Add(false); }
+        playerInput = GetComponent<PlayerInput>();
+        offsetY = transform.position.y;
+        offsetZ = (FR.position.z + FL.position.z + BR.position.z + BL.position.z) / 4;
+        legHeightOffset = transform.position.y - maxLegHeight;
     }
 
     #region
     public void LeftLegF(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed) { legActive[0] = true; }
-        if(ctx.canceled) { legActive[0] = false; }
+        if (ctx.performed) { FLB = true; }
+        if(ctx.canceled) { FLB = false; }
     }
     public void RightLegF(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed) { legActive[1] = true; }
-        if (ctx.canceled) { legActive[1] = false; }
+        if (ctx.performed) { FRB = true; }
+        if (ctx.canceled) { FRB = false; }
     }
     public void LeftLegB(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed) { legActive[2] = true; }
-        if (ctx.canceled) { legActive[2] = false; }
+        if (ctx.performed) { BLB = true; }
+        if (ctx.canceled) { BLB = false; }
     }
 
     public void RightLegB(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed) { legActive[3] = true; }
-        if (ctx.canceled) { legActive[3] = false; }
+        if (ctx.performed) { BRB = true; }
+        if (ctx.canceled) { BRB = false; }
     }
 
     #endregion
 
-    private void Update()
+    private void LateUpdate()
     {
-        int i = 0;
-        foreach (bool val in legActive)
+        FixTargets();
+        UpdateLegs();
+        UpdateBody();
+    }
+
+    void FixTargets()
+    {
+        RaycastHit hit;
+        FLgrounded = false; BLgrounded = false; FRgrounded = false; BRgrounded = false;
+        groundedLegs = 0;
+
+        if (Physics.Raycast(FL.position, -FL.up + FL.forward, out hit, 0.1f, groundLayer) && !FLB) 
         {
-            if (val) { legTargets[i].transform.localPosition += new Vector3(0, 0.03f * Time.deltaTime * legSpeed, 0); }
-            if (!val) { legTargets[i].transform.localPosition -= new Vector3(0, 0.03f * Time.deltaTime * legSpeed, 0); }
-
-            legTargets[i].position = new Vector3(legTargets[i].position.x, Mathf.Clamp(legTargets[i].position.y, -0.051f, maxLegHeight), legTargets[i].position.z);
-
-            i++;
+            FLgrounded = true; groundedLegs++;
         }
+        if (Physics.Raycast(BL.position, -BL.up + BL.forward, out hit, 0.1f, groundLayer) && !BLB)
+        {
+            BLgrounded = true; groundedLegs++;
+        }
+        if (Physics.Raycast(FR.position, -FR.up + FR.forward, out hit, 0.1f, groundLayer) && !FRB)
+        {
+            FRgrounded = true; groundedLegs++;
+        }
+        if (Physics.Raycast(BR.position, -BR.up + BR.forward, out hit, 0.1f, groundLayer) && !BRB)
+        {
+            BRgrounded = true; groundedLegs++;
+        }
+    }
+
+    void UpdateLegs()
+    {
+        if (FLB && groundedLegs > 1) { FL.position += new Vector3(0, 1 * Time.deltaTime * legSpeed, 0); } //If pressed
+        else if(!FLB && !FLgrounded) { FL.localPosition -= new Vector3(0, 1 * Time.deltaTime * legSpeed, -1 * Time.deltaTime * legSpeed) ; }
+
+        FL.position = new Vector3(FL.position.x, Mathf.Clamp(FL.position.y, -0.051f, maxLegHeight), FL.position.z);
+
+        if (BLB && groundedLegs > 1) { BL.position += new Vector3(0, 1 * Time.deltaTime * legSpeed, 0); }
+        else if (!BLB && !BLgrounded) { BL.localPosition -= new Vector3(0, 1 * Time.deltaTime * legSpeed, -1 * Time.deltaTime * legSpeed); }
+
+        BL.position = new Vector3(BL.position.x, Mathf.Clamp(BL.position.y, -0.051f, maxLegHeight), BL.position.z);
+
+        if (FRB && groundedLegs > 1) { FR.position += new Vector3(0, 1 * Time.deltaTime * legSpeed, 0); }
+        else if (!FRB && !FRgrounded) { FR.localPosition -= new Vector3(0, 1 * Time.deltaTime * legSpeed, -1 * Time.deltaTime * legSpeed); }
+
+        FR.position = new Vector3(FR.position.x, Mathf.Clamp(FR.position.y, -0.051f, maxLegHeight), FR.position.z);
+
+        if (BRB && groundedLegs > 1) { BR.position += new Vector3(0, 1 * Time.deltaTime * legSpeed, 0); }
+        else if (!BRB && !BRgrounded) { BR.localPosition -= new Vector3(0, 1 * Time.deltaTime * legSpeed, -1 * Time.deltaTime * legSpeed); }
+
+        BR.position = new Vector3(BR.position.x, Mathf.Clamp(BR.position.y, -0.051f, maxLegHeight), BR.position.z);
+    }
+
+    void UpdateBody()
+    {
+        //Move forward
+        transform.position = new Vector3(transform.position.x, ((FR.position.y + FL.position.y + BR.position.y + BL.position.y) / 4) + offsetY, ((FR.position.z + FL.position.z + BR.position.z + BL.position.z) / 4) - offsetZ);
+        maxLegHeight = transform.position.y - legHeightOffset;
+
+        //Rotate front/back
+        float averageFrontHeight = (FR.position.y + FL.position.y) / 2;
+        float averageBackHeight = (BR.position.y + BL.position.y) / 2;
+
+        float angle = Mathf.Atan2(averageFrontHeight - averageBackHeight, Vector3.Distance(transform.position, transform.position + Vector3.forward)) * Mathf.Rad2Deg;
+        Vector3 rotation = transform.rotation.eulerAngles;
+        rotation.x = angle * -rotationMultiplier;
+        transform.rotation = Quaternion.Euler(rotation);
     }
 }
