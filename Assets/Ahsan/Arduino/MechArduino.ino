24b072e1eed90bc65c0ea2352f1c343604f57ae6
ahsan2649 <ahsan2649@outlook.com>
2023-06-12 15:51:49 +0200

    Create MechArduino.ino

---
+++ Assets/Ahsan/Arduino/MechArduino.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void ButtonPress(){
+    Serial.println("Button Pressed");
+}
+
+void Potentiometer(){
+    Serial.println("Potentiometer");}
+}
+
+void LEDInput(){
+    Serial.println("LED");  
+}
