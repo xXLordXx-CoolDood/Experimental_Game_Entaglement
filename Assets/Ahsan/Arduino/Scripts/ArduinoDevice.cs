99b85e695b71bb86fcd0820aba58b0ff1980c4ec
ahsan2649 <ahsan2649@outlook.com>
2023-07-06 18:49:08 +0200

    Mappings fixed

--- Assets/Ahsan/Arduino/Scripts/ArduinoDevice.cs
+++ Assets/Ahsan/Arduino/Scripts/ArduinoDevice.cs
@@ -13,23 +13,23 @@ public struct ArduinoDeviceState : IInputStateTypeInfo
 {
     public FourCC format => new FourCC('A', 'R', 'D', 'N');
 
-    [InputControl(name = "Button1", layout = "Button", bit = 0, displayName = "Button 1")]
-    [InputControl(name = "Button2", layout = "Button", bit = 1, displayName = "Button 2")]
-    [InputControl(name = "Button3", layout = "Button", bit = 2, displayName = "Button 3")]
-    [InputControl(name = "Button4", layout = "Button", bit = 4, displayName = "Button 4")]
-    [InputControl(name = "Button5", layout = "Button", bit = 5, displayName = "Button 5")]
-    [InputControl(name = "Button6", layout = "Button", bit = 6, displayName = "Button 6")]
-    [InputControl(name = "Button7", layout = "Button", bit = 7, displayName = "Button 7")]
-    [InputControl(name = "Button8", layout = "Button", bit = 8, displayName = "Button 8")]
-    [InputControl(name = "Button9", layout = "Button", bit = 9, displayName = "Button 9")]
-    [InputControl(name = "Button10", layout = "Button", bit = 10, displayName = "Button 10")]
-    [InputControl(name = "Button11", layout = "Button", bit = 11, displayName = "Button 11")]
-    [InputControl(name = "Button12", layout = "Button", bit = 12, displayName = "Button 12")]
-    [InputControl(name = "Button13", layout = "Button", bit = 13, displayName = "Button 13")]
-    [InputControl(name = "Button14", layout = "Button", bit = 14, displayName = "Button 14")]
-    [InputControl(name = "Button15", layout = "Button", bit = 15, displayName = "Button 15")]
-    [InputControl(name = "Button16", layout = "Button", bit = 16, displayName = "Button 16")]
-    [InputControl(name = "Button17", layout = "Button", bit = 17, displayName = "Button 17")]
+    [InputControl(name = "Button1", layout = "Button", bit = 0, displayName = "Shoot Left")]
+    [InputControl(name = "Button2", layout = "Button", bit = 1, displayName = "Switch Cam")]
+    [InputControl(name = "Button3", layout = "Button", bit = 2, displayName = "Gun1Up")]
+    [InputControl(name = "Button4", layout = "Button", bit = 4, displayName = "Gun1Down")]
+    [InputControl(name = "Button5", layout = "Button", bit = 5, displayName = "Gun2Up")]
+    [InputControl(name = "Button6", layout = "Button", bit = 6, displayName = "Gun2Down")]
+    [InputControl(name = "Button7", layout = "Button", bit = 7, displayName = "Gun3Up")]
+    [InputControl(name = "Button8", layout = "Button", bit = 8, displayName = "Gun3Down")]
+    [InputControl(name = "Button9", layout = "Button", bit = 9, displayName = "Forward")]
+    [InputControl(name = "Button10", layout = "Button", bit = 10, displayName = "Reverse")]
+    [InputControl(name = "Button11", layout = "Button", bit = 11, displayName = "Shoot Right")]
+    [InputControl(name = "Button12", layout = "Button", bit = 12, displayName = "Back Right Leg")]
+    [InputControl(name = "Button13", layout = "Button", bit = 13, displayName = "Front Left Leg")]
+    [InputControl(name = "Button14", layout = "Button", bit = 14, displayName = "Front Right Leg")]
+    [InputControl(name = "Button15", layout = "Button", bit = 15, displayName = "Back Left Leg")]
+    [InputControl(name = "Button16", layout = "Button", bit = 16, displayName = "Turn Left")]
+    [InputControl(name = "Button17", layout = "Button", bit = 17, displayName = "Turn Right")]
 
 
     public int buttons;
@@ -55,33 +55,45 @@ public class ArduinoDevice : InputDevice
         InputSystem.RegisterLayout<ArduinoDevice>(matches: new InputDeviceMatcher().WithInterface("Arduino"));
     }
 
-    public ButtonControl firstButton { get; private set; }
-    public ButtonControl secondButton { get;private set; }
-    public ButtonControl thirdButton { get; private set; }
-    public ButtonControl fourthButton { get; private set; }
-    public ButtonControl fifthButton { get; private set; }
-    public ButtonControl sixthButton { get; private set; }
-    public ButtonControl seventhButton { get; private set; }
-    public ButtonControl eighthButton { get; private set; }
-    public ButtonControl ninthButton { get; private set; }
-    public ButtonControl tenthButton { get; private set; }
-    public ButtonControl eleventhButton { get; private set; }
+    public ButtonControl shootLeft { get; private set; }
+    public ButtonControl cameraSwitch { get;private set; }
+    public ButtonControl gun1up { get; private set; }
+    public ButtonControl gun1down { get; private set; }
+    public ButtonControl gun2up { get; private set; }
+    public ButtonControl gun2down { get; private set; }
+    public ButtonControl gun3up { get; private set; }
+    public ButtonControl gun3down { get; private set; }
+    public ButtonControl forward { get; private set; }
+    public ButtonControl reverse { get; private set; }
+    public ButtonControl shootRight { get; private set; }
+    public ButtonControl backRight { get; private set; }
+    public ButtonControl frontLeft { get; private set; }
+    public ButtonControl frontRight { get; private set; }
+    public ButtonControl backLeft { get; private set; }
+    public ButtonControl turnLeft { get; private set; }
+    public ButtonControl turnRight { get; private set; }
 
     protected override void FinishSetup()
     {
         base.FinishSetup();
 
-        firstButton = GetChildControl<ButtonControl>("Button1");
-        secondButton = GetChildControl<ButtonControl>("Button2");
-        thirdButton    = GetChildControl<ButtonControl>("Button3");
-        fourthButton   = GetChildControl<ButtonControl>("Button4");
-        fifthButton    = GetChildControl<ButtonControl>("Button5");
-        sixthButton    = GetChildControl<ButtonControl>("Button6");
-        seventhButton  = GetChildControl<ButtonControl>("Button7");
-        eighthButton   = GetChildControl<ButtonControl>("Button8");
-        ninthButton    = GetChildControl<ButtonControl>("Button9");
-        tenthButton    = GetChildControl<ButtonControl>("Button10");
-        eleventhButton = GetChildControl<ButtonControl>("Button11");
+        shootLeft = GetChildControl<ButtonControl>("Button1");
+        cameraSwitch = GetChildControl<ButtonControl>("Button2");
+        gun1up    = GetChildControl<ButtonControl>("Button3");
+        gun1down   = GetChildControl<ButtonControl>("Button4");
+        gun2up    = GetChildControl<ButtonControl>("Button5");
+        gun2down    = GetChildControl<ButtonControl>("Button6");
+        gun3up  = GetChildControl<ButtonControl>("Button7");
+        gun3down   = GetChildControl<ButtonControl>("Button8");
+        forward    = GetChildControl<ButtonControl>("Button9");
+        reverse    = GetChildControl<ButtonControl>("Button10");
+        shootRight = GetChildControl<ButtonControl>("Button11");
+        backRight = GetChildControl<ButtonControl>("Button12");
+        frontLeft = GetChildControl<ButtonControl>("Button13");
+        frontRight = GetChildControl<ButtonControl>("Button14");
+        backLeft = GetChildControl<ButtonControl>("Button15");
+        turnLeft = GetChildControl<ButtonControl>("Button16");
+        turnRight = GetChildControl<ButtonControl>("Button17");
     }
 
     public static ArduinoDevice current { get; private set; }
